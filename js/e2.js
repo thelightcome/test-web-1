7f3e1ca0457299416ce6edf1ddc7e5e15f9767ff
thelightcome <nurikserikbay1995@gmail.com>
2022-11-16 10:41:33 +0600

    c

--- js/e2.js
+++ js/e2.js
@@ -21,42 +21,63 @@
 
   const overflow = $('.overflow');
   const sliderWrapper = $('.slider-wrapper');
-  const height = sliderWrapper.height();
   const linkList = $('.list__el');
+  const bgColors = $('.swiper-slide').map((_, e) => {
+    return e.getAttribute('data-background');
+  });
+
   const maxValue = linkList.length;
 
-  overflow.css('height', maxValue * height + 'px');
-
   let overflowTop = overflow.offset().top;
+  let height = sliderWrapper.height();
+  overflow.css('height', (maxValue + 1) * height + 'px');
 
   let item = 0;
-  let prevScrollVal = $(window).scrollTop();
   let currentValue, shift;
   let duration = 500;
-  let endPoint = (maxValue - 1) * height;
+  let endPoint = maxValue * height;
+  let position = 'absolute';
 
-  $(window).on("onContentLoaded", scrollHandle);
+
+  $(window).on("resize", () => {
+    overflowTop = overflow.offset().top;
+    height = sliderWrapper.height();
+    overflow.css('height', (maxValue + 1) * height + 'px');
+  });
+
+  $(document).ready(function () {
+    scrollHandle();
+    if (item === 0) {
+      linkList[item].classList.add('active-el');
+      sliderWrapper.css("background-color", bgColors[item]);
+      newSlider.slideTo(item, duration);
+    }
+  });
 
   $(window).on("scroll", scrollHandle);
 
   function scrollHandle() {
     currentValue = $(window).scrollTop();
     shift = currentValue - overflowTop;
-    prevScrollVal = currentValue;
 
     if (shift > 0 && shift < endPoint) {
-      sliderWrapper.css('position', 'fixed');
-      sliderWrapper.css('top', '0px');
+      if (position === 'absolute') {
+        position = 'fixed';
+        sliderWrapper.css('position', position);
+        sliderWrapper.css('top', '0px');
+      }
       let setItem = Math.floor(shift / height);
       setSlide(setItem);
     } else {
-      sliderWrapper.css('position', 'absolute');
+      if (position === 'fixed') {
+        position = 'absolute';
+        sliderWrapper.css('position', position);
+      }
       if (shift < 0) {
         setSlide(0);
         sliderWrapper.css('top', '0px');
       } else if (shift > endPoint) {
         setSlide(maxValue - 1);
-        console.log(endPoint)
         sliderWrapper.css('top', endPoint + 'px');
       }
     }
@@ -65,12 +86,12 @@
   function setSlide(i) {
     if (i !== item) {
       linkList.removeClass('active-el');
-      linkList[i] && linkList[i].classList.add('active-el');
-      let activeSlide = document.querySelector(".swiper-slide-active");
-      let background = activeSlide.getAttribute("data-background");
-      sliderWrapper.css("background-color", background);
-      newSlider.slideTo(i, duration);
-      item = i;
+      if (linkList[i]) {
+        linkList[i].classList.add('active-el');
+        sliderWrapper.css("background-color", bgColors[i]);
+        newSlider.slideTo(i, duration);
+        item = i;
+      }
     }
   }
 }())
